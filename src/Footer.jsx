f9c4677cda4e5fc7f41f7ff8e8d76a03e66fb791
askarf <ayalakarf@gmail.com>
2024-03-21 12:15:59 -0400

    adds theme to paintingshow

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,7 +8,7 @@ export function Footer() {
     window.location.href = "/";
   };
   return (
-    <footer className="py-5 bg-dark">
+    <footer className="py-5 bg-dark flex-stick">
       <div className="container">
         <p className="m-0 text-center text-white">Copyright &copy; Yosefa 2024</p>
         {localStorage.jwt === undefined ? (

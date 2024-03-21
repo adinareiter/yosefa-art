d27fb04a8692b3cb30412c1b7c15735cd9668fd5
askarf <ayalakarf@gmail.com>
2024-03-21 12:49:01 -0400

    adds email themed form

--- src/Header.jsx
+++ src/Header.jsx
@@ -52,6 +52,11 @@ export function Header() {
           ) : (
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
               <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/admin-page">
+                  Admin Page
+                </a>
+              </li>
+              <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
